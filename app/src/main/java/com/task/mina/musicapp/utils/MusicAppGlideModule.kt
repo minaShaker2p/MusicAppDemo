4761edc7e8332c7695f7cc1d30f4a11ac3b19147
Mina shaker <minashakeralfy@gmail.com>
2019-02-11 00:36:49 +0200

    create music app project and add all dependencies

---
+++ app/src/main/java/com/task/mina/musicapp/utils/MusicAppGlideModule.kt
@@ -0,0 +1,11 @@
+package com.task.mina.musicapp.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Mina Alfy on 2/11/2019.
+ */
+@GlideModule
+class MusicAppGlideModule : AppGlideModule() {
+}
