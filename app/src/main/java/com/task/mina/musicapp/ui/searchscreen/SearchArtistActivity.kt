d55da6e502becb6db1e6f4d0329b59dfb5e2c7af
Mina shaker <minashakeralfy@gmail.com>
2019-02-14 21:17:10 +0200

    create artist list adapter

--- app/src/main/java/com/task/mina/musicapp/ui/searchscreen/SearchArtistActivity.kt
+++ app/src/main/java/com/task/mina/musicapp/ui/searchscreen/SearchArtistActivity.kt
@@ -5,8 +5,6 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.widget.Toast
 import com.task.mina.musicapp.R
 import com.task.mina.musicapp.base.presentation.viewmodel.ViewModelFactory
 import dagger.android.AndroidInjection
