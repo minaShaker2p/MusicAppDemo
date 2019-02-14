2080df6b35376bc0d1d35e351b0794e391c5783d
Mina shaker <minashakeralfy@gmail.com>
2019-02-14 21:53:09 +0200

    add Top artist albums activity

---
+++ app/src/main/java/com/task/mina/musicapp/ui/topablums/TopArtistAlbumsActivity.kt
@@ -0,0 +1,13 @@
+package com.task.mina.musicapp.ui.topablums
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.task.mina.musicapp.R
+
+class TopArtistAlbumsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_artist_albums)
+    }
+}
